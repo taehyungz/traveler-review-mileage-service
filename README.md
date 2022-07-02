6151e1c384fd385029b8a37040a3b267c1f21df2
taehyungz <thkthk97@naver.com>
2022-07-02 19:48:45 +0900

    docs: README 작성

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # traveler-review-mileage-service
+사용자들이 장소에 리뷰를 작성할 때 포인트를 부여하고, 전체/개인에 대한 포인트 부여 및 히스토리와 개인별 누적 포인트 관리하는 서비스
+
+
+## 실행 방법
+### 프로젝트 클론
+> cd ~
+
+> git clone https://github.com/taehyungz/traveler-review-mileage-service.git
+
+### 프로젝트로 이동
+> cd ./traveler-review-mileage-service
+  
+### 도커 콤포즈 실행
+> cd ./docker-compose
+  
+> docker-compose -p mileage up -d
+  
+### 스프링 부트 프로젝트 실행  
+프로젝트 빌드
+> cd ../
+
+> ./gradlew bootJar
+
+프로젝트 실행
+> cd ./build/libs
+
+> java -jar build/libs/mileage-0.0.1-SNAPSHOT.jar
+
+
+## 프로젝트 설명
+### 기능
+이벤트 발생을 구독하여 포인트 관련 서비스 기능을 제공합니다.  
+- 포인트 적립 API [POST /events]
+- 포인트 조회 API
+  - 개인의 누적 포인트 조회 [GET /api/v1/users/points]
+  - 개인의 누적 포인트와 포인트 변동 이력 조회[GET /api/v1/users/point-events]
+
+### 스키마
+[최종 스키마 sql](https://github.com/taehyungz/traveler-review-mileage-service/blob/main/schema.sql)
---
+++ schema.sql
@@ -0,0 +1,64 @@
+CREATE TABLE point
+(
+    point_id   BIGINT AUTO_INCREMENT NOT NULL,
+    user_id    VARCHAR(36)           NOT NULL,
+    amount     INT                   NOT NULL,
+    version    BIGINT                NOT NULL,
+    created_at datetime              NOT NULL,
+    updated_at datetime              NOT NULL,
+    CONSTRAINT pk_point PRIMARY KEY (point_id)
+);
+
+ALTER TABLE point
+    ADD CONSTRAINT uc_point_userid UNIQUE (user_id);
+
+
+CREATE TABLE point_event
+(
+    point_event_id BIGINT AUTO_INCREMENT NOT NULL,
+    point_id       BIGINT                NOT NULL,
+    review_id      VARCHAR(36)           NOT NULL,
+    user_id        VARCHAR(36)           NOT NULL,
+    action_type    VARCHAR(32)           NOT NULL,
+    reason         VARCHAR(255)          NOT NULL,
+    amount         INT                   NOT NULL,
+    version        BIGINT                NOT NULL,
+    created_at     datetime              NOT NULL,
+    updated_at     datetime              NOT NULL,
+    CONSTRAINT pk_point_event PRIMARY KEY (point_event_id)
+);
+
+CREATE INDEX idx_point_event_review_id ON point_event (review_id);
+
+CREATE INDEX idx_point_event_user_id ON point_event (user_id);
+
+ALTER TABLE point_event
+    ADD CONSTRAINT FK_POINT_EVENT_ON_POINT FOREIGN KEY (point_id) REFERENCES point (point_id);
+
+
+CREATE TABLE review
+(
+    review_id  VARCHAR(36) NOT NULL,
+    user_id    VARCHAR(36) NOT NULL,
+    place_id   VARCHAR(36) NOT NULL,
+    created_at datetime    NOT NULL,
+    updated_at datetime    NOT NULL,
+    CONSTRAINT pk_review PRIMARY KEY (review_id)
+);
+
+CREATE INDEX idx_review_user_id ON review (user_id);
+
+CREATE INDEX idx_review_place_id ON review (place_id);
+
+
+CREATE TABLE photo
+(
+    photo_id   VARCHAR(36) NOT NULL,
+    review_id  VARCHAR(36) NOT NULL,
+    created_at datetime    NOT NULL,
+    updated_at datetime    NOT NULL,
+    CONSTRAINT pk_photo PRIMARY KEY (photo_id)
+);
+
+ALTER TABLE photo
+    ADD CONSTRAINT FK_PHOTO_ON_REVIEW FOREIGN KEY (review_id) REFERENCES review (review_id);
